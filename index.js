ee8e25c8a6a42ce70bc4caae7a84a1808ce85eb7
CaanBeef <157432724+CaanBeef@users.noreply.github.com>
2024-04-05 09:38:35 -0400

    Literally refactored everything :3

--- index.js
+++ index.js
@@ -1,117 +1,145 @@
-let coffee = 0;
-let funds = 0;
-let BrewerCost = 5;
-let BaristaCost = 5;
-let brewers = 0;
-let baristas = 0;
-let CoffeeMult = 1;
-let SellMult = 1;
-let Upgrade1Cost = 100;
-let Upgrade2Cost = 100;
-let RandomConsoleStuff = ["Why are you in here?","Cheating isnt very nice","Haii"];
-//TODO Document func and like yk make it look not doodoo
+let RandomConsoleStuff = ["Ballmaster32", ":3", "Haii"];
 
-document.getElementById("MakeCoffee").onclick = () => {
-  coffee = coffee + CoffeeMult;
+const Game = {
+  coffee: 0,
+  funds: 0,
+  BrewerCost: 5,
+  BaristaCost: 5,
+  brewers: 0,
+  baristas: 0,
+  CoffeeMult: 1,
+  SellMult: 1,
+  Upgrade1Cost: 100,
+  Upgrade2Cost: 100,
+  TotalFundsGen: 0,
 };
 
-document.getElementById("SellCoffee").onclick = () => {
-  if (coffee >= 0 + SellMult) {
-    funds = funds + SellMult;
-    coffee = coffee - SellMult;
+function debugObjectValues(obj) {
+  for (const key in obj) {
+    console.log(`${key}: ${obj[key]}`);
   }
-};
+}
 
-document.getElementById("BuyBrewer").onclick = () => {
-  if (funds - BrewerCost >= 0) {
-    funds = funds - BrewerCost;
-    brewers++;
-    BrewerCost = BrewerCost * 1.15;
+document.addEventListener("keydown", (event) => {
+  if (event.key == "z") {
+    debugObjectValues(Game);
   }
-};
+});
 
-document.getElementById("HireBarista").onclick = () => {
-  if (funds - BaristaCost >= 0) {
-    funds = funds - BaristaCost;
-    baristas++;
-    BaristaCost = BaristaCost * 1.15;
+// Function to handle button clicks
+function handleClick(id, callback) {
+  document.getElementById(id).onclick = callback;
+}
+
+// Function to handle purchasing items
+function purchaseItem(cost, costMultiplier) {
+  if (Game.funds - cost >= 0) {
+    Game.funds -= cost;
+    cost *= costMultiplier;
   }
-};
+}
 
-document.getElementById("UpgradeFilter").onclick = () => {
-  if (funds - Upgrade1Cost >= 0) {
-    funds = funds - Upgrade1Cost;
-    CoffeeMult++;
-    Upgrade1Cost = Upgrade1Cost * 1.325;
+// Event handlers for button clicks
+handleClick("MakeCoffee", () => {
+  Game.coffee += Game.CoffeeMult;
+});
+
+handleClick("SellCoffee", () => {
+  if (Game.coffee >= 0 + Game.SellMult) {
+    Game.TotalFundsGen += Game.SellMult;
+
+    Game.funds += Game.SellMult;
+    Game.coffee -= Game.SellMult;
   }
-};
+});
 
+handleClick("BuyBrewer", () => {
+  purchaseItem(Game.BrewerCost, 1.15);
+  Game.brewers++;
+});
 
-document.getElementById("UpgradeMarketing").onclick = () => {
-  if (funds - Upgrade2Cost >= 0) {
-    funds = funds - Upgrade2Cost;
-    SellMult++;
-    Upgrade2Cost = Upgrade2Cost * 1.325;
+handleClick("HireBarista", () => {
+  purchaseItem(Game.BaristaCost, 1.15);
+  Game.baristas++;
+});
+
+handleClick("UpgradeFilter", () => {
+  if (Game.funds - Game.Upgrade1Cost >= 0) {
+    Game.funds -= Game.Upgrade1Cost;
+    Game.CoffeeMult++;
+    Game.Upgrade1Cost *= 1.325;
   }
-};
+});
 
-
-setInterval(() => {
-  if (brewers > baristas) {
-    document.getElementById("CoffeeLabel").innerText =
-      "Coffee's: " +
-      coffee.toLocaleString(1) +
-      " (+" +
-      (brewers - baristas) +
-      " /s)";
-  } else if (baristas > brewers) {
-    document.getElementById("CoffeeLabel").innerText =
-      "Coffee's: " +
-      coffee.toLocaleString(1) +
-      " (" +
-      (brewers - baristas) +
-      " /s)";
-  } else {
-    document.getElementById("CoffeeLabel").innerText =
-      "Coffee's: " +
-      coffee.toLocaleString(1) +
-      " (+" +
-      (brewers - baristas) +
-      " /s)";
+handleClick("UpgradeMarketing", () => {
+  if (Game.funds - Game.Upgrade2Cost >= 0) {
+    Game.funds -= Game.Upgrade2Cost;
+    Game.SellMult++;
+    Game.Upgrade2Cost *= 1.325;
   }
+});
 
-  if (baristas) {
-    document.getElementById("Funds").innerText =
-      "Funds: $" + funds.toLocaleString(1) + " (+" + baristas + " /s)";
-  } else {
-    document.getElementById("Funds").innerText =
-      "Funds: $" + funds.toLocaleString(2);
-  }
+// Function to update Coffee label
+function updateCoffeeLabel() {
+  const coffeeDifference = Game.brewers - Game.baristas;
+  const coffeePerSecond =
+    coffeeDifference > 0 ? "+" + coffeeDifference : coffeeDifference;
+  const coffeeText =
+    "Coffee's: " +
+    Game.coffee.toLocaleString(1) +
+    " (" +
+    coffeePerSecond +
+    " /s)";
+  document.getElementById("CoffeeLabel").innerText = coffeeText;
+}
 
+// Function to update Funds label
+function updateFundsLabel() {
+  const fundsText = Game.baristas
+    ? "Funds: $" + Game.funds.toLocaleString(1) + " (+" + Game.baristas + " /s)"
+    : "Funds: $" + Game.funds.toLocaleString(2);
+  document.getElementById("Funds").innerText = fundsText;
+}
+
+// Function to update various labels
+function updateLabels() {
+  // Update amount of Brewers and Baristas
   document.getElementById("AmntBrewers").innerText =
-    "You own " + brewers + " Brewers";
+    "You own " + Game.brewers + " Brewers";
   document.getElementById("AmntBaristas").innerText =
-    "You have " + baristas + " baristas";
+    "You have " + Game.baristas + " baristas";
 
+  // Update Brewer and Barista cost texts
   document.getElementById("BrewerCost").innerText =
-    "Cost: " + BrewerCost.toLocaleString(1) + "$";
+    "Cost: " + Game.BrewerCost.toLocaleString(1) + "$";
   document.getElementById("BaristaCost").innerText =
-    "Cost: " + BaristaCost.toLocaleString(1) + "$";
+    "Cost: " + Game.BaristaCost.toLocaleString(1) + "$";
+
+  // Update Upgrade labels
   document.getElementById("UpgradeFilterLabel").innerText =
-    "Cost: " + Upgrade1Cost.toLocaleString(1) + "$";
-    document.getElementById("UpgradeMarketingLabel").innerText =
-    "Cost: " + Upgrade2Cost.toLocaleString(1) + "$";
-}, 50);
-//yeah idk
-//every 1s apply automations
+    "Cost: " + Game.Upgrade1Cost + "$";
+  document.getElementById("UpgradeMarketingLabel").innerText =
+    "Cost: " + Game.Upgrade2Cost + "$";
+}
+
+// Interval function to update labels
 setInterval(() => {
-  coffee = coffee + brewers;
-  if (coffee >= 0 + baristas) {
-    coffee = coffee - baristas;
-    funds = funds + baristas;
+  updateCoffeeLabel();
+  updateFundsLabel();
+  updateLabels();
+}, 50);
+
+// Function to update coffee and funds every second
+setInterval(() => {
+  Game.coffee += Game.brewers;
+  if (Game.coffee >= 0 + Game.baristas) {
+    Game.coffee -= Game.baristas;
+    Game.funds += Game.baristas;
   }
 }, 1000);
 
-setInterval(() => {
-  console.log(RandomConsoleStuff[(Math.floor(Math.random() * RandomConsoleStuff.length))]);
-}, 5000)
+// Function to log random console stuff on page load
+window.addEventListener("load", () => {
+  const randomIndex = Math.floor(Math.random() * RandomConsoleStuff.length);
+  console.log(RandomConsoleStuff[randomIndex]);
+});
